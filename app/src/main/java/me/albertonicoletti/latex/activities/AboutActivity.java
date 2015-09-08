da7579c40369a883c81012eae7b7c836ac4da61b
Alberto Nicoletti <albyx.n@gmail.com>
2015-09-08 17:38:49 +0200

    Added about links

--- app/src/main/java/me/albertonicoletti/latex/activities/AboutActivity.java
+++ app/src/main/java/me/albertonicoletti/latex/activities/AboutActivity.java
@@ -2,6 +2,8 @@ package me.albertonicoletti.latex.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import me.albertonicoletti.latex.R;
 
@@ -16,6 +18,12 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        TextView profile = (TextView) findViewById(R.id.github_profile);
+        TextView editor = (TextView) findViewById(R.id.github_editor);
+        TextView server = (TextView) findViewById(R.id.github_server);
+        profile.setMovementMethod(LinkMovementMethod.getInstance());
+        editor.setMovementMethod(LinkMovementMethod.getInstance());
+        server.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
